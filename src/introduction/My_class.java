25c998c02dbd104f5d3cf8087805eaa485b9d09f
tejaspr31 <tejaspr31@gmail.com>
2022-04-12 00:52:40 +0530

    1st commit

---
+++ src/introduction/My_class.java
@@ -0,0 +1,12 @@
+package introduction;
+
+public class My_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//1.
+		System.out.println("my name is tejas");
+	}
+
+}
+
---
+++ src/method/Example1.java
@@ -0,0 +1,25 @@
+package method;
+
+public class Example1 {
+
+	public static void test1()
+	{
+		System.out.println("my name is tejas");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Running main method");
+		test2();
+		test1();
+		System.out.println("Ending main method");
+	}
+
+	public static void test2()
+	{
+		System.out.println("This is static regular test2 method");
+	}
+	
+}
